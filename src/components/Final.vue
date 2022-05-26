ff7bbb6712228584f72856f7169140e8e3889de4
Samuel Hosken <samuelhosken.o@gmail.com>
2022-05-26 20:28:25 -0300

    All

---
+++ src/components/Final.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="final">
+    <div :class="`final-texto ${etapa}`">Parabéns :)</div>
+    <button class="final-botao" v-on:click="jogarNovamente">
+      Jogar novamente
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Final",
+  props: {
+    etapa: String,
+    texto: String,
+    jogarNovamante: Function,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+  components: {},
+};
+</script>
+
+<style scoped>
+.final-texto {
+  margin: 20px 0px;
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.final-texto.enforcado {
+  color: rgb(228, 30, 63);
+}
+
+.final-texto.ganhador {
+  color: rgb(46, 221, 99);
+}
+</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="formulario">
+    <div class="formulario-tilte">{{ title }}</div>
+    <input type="text" class="formulario-input" v-model="inputValue" />
+    <button :disabled="inputValue.length === 0" v-on:click="onSubmit">
+      {{ button }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Formulario",
+  props: {
+    title: String,
+    button: String,
+    action: Function,
+  },
+  data() {
+    return {
+      inputValue: "",
+    };
+  },
+  methods: {
+    onSubmit: function () {
+      this.action(this.inputValue);
+      this.inputValue = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.formulario {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.formulario-input {
+  padding: 10px 5px;
+  border: 0;
+  border-radius: 5px;
+  margin: 10px 10px;
+  text-align: center;
+  color: #2c3e50;
+  font-size: 16px;
+}
+</style>
